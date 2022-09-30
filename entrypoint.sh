aa67fb560ed588abafe850487edd42c8d7c95930
Debabrata1980 <105878261+Debabrata1980@users.noreply.github.com>
2022-09-30 20:44:41 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,9 +19,20 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
+exclude_unittest_cmd=""
+if [ "${1}" != "" ]; then
+  echo "Excluding the following folders from python linting and unit testing: ${1}"
+  # The flake8 --exclude command takes a comma separated string
+  exclude_lint_cmd="--exclude ${1}"
+  # The pytest --ignore can only have one folder, so we have to repeat the flag.
+  for folder in $(echo ${1} | tr ',' ' '); do
+    exclude_unittest_cmd="--ignore ${folder} ${exclude_unittest_cmd}"
+  done
+fi
+
 # Execute unit tests using pytest, which will also execute any tests implemented using python's unittest.
 # We're executing this at the root, which will recurse the directories discovering any tests based on the
 # following best practice guidelines:
 # https://docs.pytest.org/en/7.1.x/explanation/goodpractices.html#conventions-for-python-test-discovery
 echo -e "\nExecuting unit tests..."
-python -m pytest -s
+python -m pytest -s ${exclude_unittest_cmd}
